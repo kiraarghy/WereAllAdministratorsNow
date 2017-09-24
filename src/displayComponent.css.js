4ac9fde82d130f2f4c6c60ce5ac7e9b91e5aea15
shtekhler <karaestubbs@gmail.com>
2017-09-24 01:02:24 +0100

    Day's styling done

--- src/displayComponent.css.js
+++ src/displayComponent.css.js
@@ -15,19 +15,19 @@ export default {
   },
   box: {
     color: "hsl(29, 49%, 84%)",
-    margin: "10px",
+    margin: "10px 15px 10px 15px",
     background:
     "linear-gradient(to right, rgba(39,32,25,1) 0%,rgba(48,38,30,0.5) 10%,rgba(46,36,28,0.49) 20%,rgba(62,48,38,0.38) 30%,rgba(62,48,38,0.18) 40%)"
   },
   box2: {
     color: "hsl(29, 49%, 84%)",
-    margin: "15px"
+    margin: "0px 15px 0px 15px",
+    background: "linear-gradient(to right, rgba(39,32,25,1) 0%,rgba(48,38,30,0.5) 10%,rgba(46,36,28,0.49) 20%,rgba(62,48,38,0.38) 30%,rgba(62,48,38,0.18) 40%)",
+  },
+  containerStyle : {
+    marginBottom: "10px",
   },
   horizontalLine: {
     color: "grey"
   },
-  gradient: {
-    background:
-      "linear-gradient(to right, rgba(39,32,25,1) 0%,rgba(48,38,30,0.5) 10%,rgba(46,36,28,0.49) 20%,rgba(62,48,38,0.38) 30%,rgba(62,48,38,0.18) 40%)"
-  }
 };
--- src/displayComponent.js
+++ src/displayComponent.js
@@ -1,6 +1,10 @@
 import React from "react";
 import styles from "./displayComponent.css.js";
 
+import Achievement10 from "./200px-Class_K_fire_icon.png";
+import Achievement20 from "./200px-Fire_type_A.png";
+import Achievement40 from "./200px-Fire_type_B.png";
+
 class DisplayComponent extends React.Component {
   emailsPerHourContainter = () => {
     if (
@@ -23,6 +27,17 @@ class DisplayComponent extends React.Component {
       return <div style={styles.box2}>Their rank is: {this.props.rank}.</div>;
     }
   };
+  achievementContainer = () => {
+    if (this.props.noEmails >= 10 && this.props.noEmails <= 20) {
+      return <img src={Achievement10} width={50} height={50} alt={this.props.achievementAlt} />;
+    }
+    if (this.props.noEmails >= 21 && this.props.noEmails <= 30) {
+      return <img src={Achievement20} width={50} height={50} alt={this.props.achievementAlt} />;
+    }
+    if (this.props.noEmails >= 31) {
+      return <img src={Achievement40} width={50} height={50} alt={this.props.achievementAlt} />;
+    }
+  };
 
   render() {
     return (
@@ -31,10 +46,11 @@ class DisplayComponent extends React.Component {
           <div style={styles.box}>
             {this.props.name} sent: {this.props.noEmails} emails today.
           </div>
-          <div style={styles.gradient}>
+          <div style={styles.containerStyle}>
             {this.emailsPerHourContainter()}
             {this.rankContainer()}
           </div>
+          <div>{this.achievementContainer()}</div>
         </div>
       </div>
     );
